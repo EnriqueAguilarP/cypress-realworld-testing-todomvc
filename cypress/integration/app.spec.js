40d4e0b2fdf4fab804d226d19a839910e67c64f4
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-25 19:53:07 -0500

    Create a test 2: add three todos

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,23 @@
 describe("React TodoMVC", () => {
-    it("adds a single todo", () => {
+    //Refactor the names of out todos into constants
+    const TODO_ITEM_ONE = 'Buy Milk'
+    const TODO_ITEM_TWO = 'Pay Rent'
+    const TODO_ITEM_THREE = 'Pickup Dry Cleaning'
+    //Mocha function that execute code before every test, in this case for URL
+    beforeEach(() => {
         cy.visit("http://localhost:8888")
-        cy.get(".new-todo").type("Buy Milk{enter}")
+    })
+
+    it("adds a single todo", () => {
+        
+        cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
         cy.get(".todo-list li").should("have.length", 1)
+    })
+
+    it("add three todos", () => {
+        cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
+        cy.get(".new-todo").type(`${TODO_ITEM_TWO}{enter}`)
+        cy.get(".new-todo").type(`${TODO_ITEM_THREE}{enter}`)
+        cy.get(".todo-list li").should("have.length", 3)
     })
 })
