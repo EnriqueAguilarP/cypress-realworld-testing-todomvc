612c5b7d176cd929dffa89d3e706d55203759530
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-26 10:55:05 -0500

    Add cypress command log into the test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -16,15 +16,18 @@ describe("React TodoMVC", () => {
         cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
     })
 
+    it.only("add three todos", () => {
+        cy.createDefaultTodos()
+        cy.get(".todo-list li").should("have.length", 3)
+    })
+
     it("should append new items to the botton of the list", () => {
         cy.createDefaultTodos()
         cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
         cy.get(".todo-list li").eq(1).find("label").should("contain", TODO_ITEM_TWO)
         cy.get(".todo-list li").eq(2).find("label").should("contain", TODO_ITEM_THREE)
+        cy.get(".todo-count").contains("3 items left")
     })
 
-    it("add three todos", () => {
-        cy.createDefaultTodos()
-        cy.get(".todo-list li").should("have.length", 3)
-    })
+    
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,16 @@ Cypress.Commands.add("createDefaultTodos", () => {
     const TODO_ITEM_ONE = "Buy Milk"
     const TODO_ITEM_TWO = "Pay Rent"
     const TODO_ITEM_THREE = "Pickup Dry Cleaning"
-  
+
+    Cypress.log({
+      name: "create default todos",
+      consoleProps() {
+        return {
+          "Inserted Todos": [TODO_ITEM_ONE, TODO_ITEM_TWO, TODO_ITEM_THREE],
+        }
+      },
+    })
+    
     cy.get(".new-todo")
       .type(`${TODO_ITEM_ONE}{enter}`)
       .type(`${TODO_ITEM_TWO}{enter}`)
